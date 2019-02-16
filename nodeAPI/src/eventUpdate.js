98651f3f7dc3d72e6d0e9ba5096e719a24e8f435
Edward Gervis <edward.gervis@gmail.com>
2019-02-16 15:02:06 -0500

    eventUpdate

---
+++ nodeAPI/src/eventUpdate.js
@@ -0,0 +1,44 @@
+"use strict";
+import { events } from 'prototypes';
+
+export const request = async (req, res, pool) => {
+  try {
+    // Get the user by email and password
+    let query = {
+      text: 'SELECT * FROM events'
+    };
+    let events = await pool.query(query);
+    if (events) {
+      res.status(200);
+      let eventRows = user.rows;
+      let array = [];
+      foreach (row in eventRows)
+      {
+        currentEvent = events(row.eventid, '', '', row.dateexpires, ''));
+        let mydate = new Date(currentEvent.dateexpires);
+        let currentDate = new Date();
+        let newDate = new Date(mydate.setTime( mydate.getTime() + 1 * 86400000 ));
+        if(currentDate<newDate)
+        {
+          array.push(currentEvent)
+        }
+      }
+      foreach (date in array)
+      {
+        let eventID = date.eventid;
+        let query2 = {
+          text: 'DELETE FROM events WHERE eventid = $1',
+          values: [eventID]
+        };
+        await pool.query(query2);
+      }
+    } else {
+      res.status(404);
+      response = {};
+    }
+    res.send(JSON.stringify(response));
+  } catch (error) {
+    console.error('ERROR getting user', error.stack);
+    res.status(500).send({'error': error.stack});
+  }
+}
