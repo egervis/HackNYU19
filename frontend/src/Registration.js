ead61e467d66ecebbfa1026deb844b27192f5f45
HKgem1314 <46288591+HKgem@users.noreply.github.com>
2019-02-16 11:32:51 -0500

    Create Registration.js

---
+++ frontend/src/Registration.js
@@ -0,0 +1,15 @@
+-import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import App from './App';
+
+class Registration extends Component {
+  render() {
+    return (
+      <div className="Registration">
+        <h1>Registration</h1>
+      </div>
+    );
+  }
+}
+
+export default Registration;
