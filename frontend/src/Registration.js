e05fea88d772b308320026078d31f04007d84bd8
HKgem1314 <46288591+HKgem@users.noreply.github.com>
2019-02-16 13:27:44 -0500

    Update Registration.js

--- frontend/src/Registration.js
+++ frontend/src/Registration.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import App from './App';
 import './styles/Registration.css';
 
