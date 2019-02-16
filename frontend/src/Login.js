84853e1e16302104ac48249503a669acad7fce57
Tracey <zixuan.kong70@myhunter.cuny.edu>
2019-02-16 16:16:53 -0500

    added cookies

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -58,7 +58,8 @@ class Login extends Component{
 
           <h1 class="mt-5 pt-3">We're here to help you learn!</h1>
           <div class="container-fluid w-25 jumbotron mt-5 bg-dark">
-            <LoginForm />
+            <LoginForm name={name} onChange={this.handleNameChange.bind(this)}/>
+            {this.state.name && <h1>Hello {this.state.name}!</h1>}
           </div>
         </div>
       )
@@ -66,3 +67,20 @@ class Login extends Component{
 }
 
 export default withCookies(Login);
+
+// <form>
+//   <h2 class="pb-3">Login</h2>
+//   <div class="form-group">
+//     <label for="emailInput">Email address</label>
+//     <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Your email"/>
+//   </div>
+//   <div class="form-group">
+//     <label for="pwInput">Password</label>
+//     <input type="password" class="form-control" id="pwInput" placeholder="Password"/>
+//   </div>
+//   <div class="form-check">
+//     <input type="checkbox" class="form-check-input" id="remember"/>
+//     <label class="form-check-label" for="remember">Remember me</label>
+//   </div>
+//   <button type="submit" class="btn btn-success mt-3">Login</button>
+// </form>
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -3,39 +3,34 @@ import React, { Component } from 'react';
 class LoginForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      name: ''
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({name: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('An email was submitted: ' + this.state.value);
+    alert('An name was submitted: ' + this.state.name);
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <h2 class="pb-3">Login</h2>
-        <div class="form-group">
-          <label for="emailInput">Email address</label>
-          <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Your email"/>
-        </div>
-        <div class="form-group">
-          <label for="pwInput">Password</label>
-          <input type="password" class="form-control" id="pwInput" placeholder="Password"/>
-        </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="remember"/>
-          <label class="form-check-label" for="remember">Remember me</label>
-        </div>
-        <button type="submit" class="btn btn-success mt-3">Login</button>
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
       </form>
-    )
+    );
   }
 }
+
+export default LoginForm;
