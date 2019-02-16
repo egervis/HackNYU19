93b61f092495dbd4b751137e1dbc1bc9b02f5a93
Edward Gervis <edward.gervis@gmail.com>
2019-02-15 21:21:34 -0500

    sql updated

--- db/initdb/init.sql
+++ db/initdb/init.sql
@@ -6,7 +6,7 @@ CREATE DATABASE sampledb
 
 CREATE TABLE Users (
     userID int NOT NULL PRIMARY KEY,
-    type int, /* 0 for student, 1 for teacher*/
+    userType int, /* 0 for student, 1 for teacher*/
     lastName varchar(255),
     firstName varchar(255),
     email varchar(255),
@@ -15,22 +15,22 @@ CREATE TABLE Users (
 
 CREATE TABLE Classes (
     classCRN int,
-    name varchar(255),
+    className varchar(255),
     studentIDs text,
     instructorID int REFERENCES Users(userID)
 );
 
 CREATE TABLE Events (
     eventID int NOT NULL PRIMARY KEY,
-    type varchar(255),
-    name varchar(255),
+    eventType varchar(255),
+    eventName varchar(255),
     dateExpires varchar(255), /*when the date gets passed gets deleted from table.*/
     instructorID int REFERENCES Users(userID)
 );
 
 CREATE TABLE Lessons (
     lessonID int NOT NULL PRIMARY KEY,
-    name varchar(255),
+    lessonName varchar(255),
     lessonDescription text,
     pictureIds text, /*picture ids*/
     instructorID int REFERENCES Users(userID)
@@ -38,6 +38,6 @@ CREATE TABLE Lessons (
 
 CREATE TABLE Pictures (
     pictureID int NOT NULL PRIMARY KEY,
-    name varchar(255),
+    pictureName varchar(255),
     lessonID int REFERENCES Lessons(lessonID)
 );
