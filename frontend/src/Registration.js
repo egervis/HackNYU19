85b58f0e8592f8f331e35d1e094efacb32a9fc6f
Tracey <zixuan.kong70@myhunter.cuny.edu>
2019-02-16 11:35:43 -0500

    created teacher folder for teacher pages

--- frontend/src/Registration.js
+++ frontend/src/Registration.js
@@ -1,4 +1,4 @@
--import React, { Component } from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from './App';
 
