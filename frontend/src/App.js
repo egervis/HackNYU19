208cae97db3fb14d66b7cb3088b5da222cfd6908
HKgem1314 <46288591+HKgem@users.noreply.github.com>
2019-02-16 00:28:20 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,43 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav class="navbar navbar-expand-md fixed-top bg-dark navbar-dark py-2">
+          <a class="navbar-brand" href="#">Company Name</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="registration.html">Register</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <h1 class="mt-5 pt-5" style="text-align:center; font-family:'Lobster', sans-serif;">We're here to help you learn!</h1>
+
+        <div class="container-fluid w-25 jumbotron mt-5 bg-dark">
+          <form>
+            <h2 class="pb-3">Login</h2>
+            <div class="form-group">
+              <label for="emailInput">Email address</label>
+              <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Your email">
+            </div>
+            <div class="form-group">
+              <label for="pwInput">Password</label>
+              <input type="password" class="form-control" id="pwInput" placeholder="Password">
+            </div>
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" id="remember">
+              <label class="form-check-label" for="remember">Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-success mt-3">Login</button>
+          </form>
+        </div>
       </div>
     );
   }
