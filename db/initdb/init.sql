6914faa25ae0f51edca2ba843a85d5d5d4876670
Edward Gervis <edward.gervis@gmail.com>
2019-02-16 10:30:48 -0500

    sql edit

--- db/initdb/init.sql
+++ db/initdb/init.sql
@@ -5,7 +5,7 @@ CREATE DATABASE sampledb
     CONNECTION LIMIT = -1;
 
 CREATE TABLE Users (
-    userID int NOT NULL PRIMARY KEY,
+    userID text NOT NULL PRIMARY KEY,
     userType int, /* 0 for student, 1 for teacher*/
     lastName varchar(255),
     firstName varchar(255),
@@ -15,30 +15,30 @@ CREATE TABLE Users (
 );
 
 CREATE TABLE Classes (
-    classCRN int NOT NULL PRIMARY KEY,
+    classID text NOT NULL PRIMARY KEY,
     className varchar(255),
     studentIDs text,
-    instructorID int REFERENCES Users(userID)
+    instructorID text REFERENCES Users(userID)
 );
 
 CREATE TABLE Events (
-    eventID int NOT NULL PRIMARY KEY,
+    eventID text NOT NULL PRIMARY KEY,
     eventType varchar(255),
     eventName varchar(255),
     dateExpires varchar(255), /*when the date gets passed gets deleted from table.*/
-    instructorID int REFERENCES Users(userID)
+    instructorID text REFERENCES Users(userID)
 );
 
 CREATE TABLE Lessons (
-    lessonID int NOT NULL PRIMARY KEY,
+    lessonID text NOT NULL PRIMARY KEY,
     lessonName varchar(255),
     lessonDescription text,
     pictureIds text, /*picture ids*/
-    instructorID int REFERENCES Users(userID)
+    instructorID text REFERENCES Users(userID)
 );
 
 CREATE TABLE Pictures (
-    pictureID int NOT NULL PRIMARY KEY,
+    pictureID text NOT NULL PRIMARY KEY,
     pictureName varchar(255),
-    lessonID int REFERENCES Lessons(lessonID)
+    lessonID text REFERENCES Lessons(lessonID)
 );
