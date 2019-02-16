8ef9f556e7b8d0847086ec82bb5b8a9fe509ddfa
Brian Chiu <bkchiu0@gmail.com>
2019-02-15 23:26:01 -0500

    Setup the structure

--- nodeAPI/src/endpointMapper.js
+++ nodeAPI/src/endpointMapper.js
@@ -1,5 +1,8 @@
-// Maps endpoints their respective functions
+"use strict";
 
-const endpoints = {
-  
+import * as init from './init';
+
+// Maps endpoints their respective functions
+export const endpoints = {
+  init: init.request
 }
