3faecaaee012f4b397304618d59afcb322e34bfd
Edward Gervis <edward.gervis@gmail.com>
2019-02-16 10:27:15 -0500

    user prototype

---
+++ nodeAPI/src/prototypes.js
@@ -0,0 +1,10 @@
+function users(userid, usertype, lastname, firstname, email, password, eventids)
+{
+  this.userid = userid;
+  this.usertype = usertype;
+  this.lastname = lastname;
+  this.firstname = firstname;
+  this.email = email;
+  this.password = password;
+  this.eventids = eventids;
+}
