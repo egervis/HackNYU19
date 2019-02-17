4b189ea4216508011cc08bfaa499e6ebb8db0642
HKgem1314 <46288591+HKgem@users.noreply.github.com>
2019-02-17 04:07:16 -0500

    Update feedback.js

--- frontend/src/teacher/feedback.js
+++ frontend/src/teacher/feedback.js
@@ -3,11 +3,34 @@ import Burger from '../components/Burger';
 import '../styles/Burger.css';
 
 class TeacherFeedback extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: ''
+    };
+
+    this.fetchFeedback = this.fetchFeedback.bind(this);
+  }
+
+  fetchFeedback() {
+    let feedback = feedbackRequestor(localStorage.getItem('userType'), localStorage.getItem('userid'));
+    return feedback;
+  }
+
   render(){
+    let temp = <></>
+    if(localStorage.getItem('userType') === '1'){
+      temp = (
+        <div class="mx-auto w-75 bg-dark my-5 px-5 py-5">
+        </div>
+      )
+    }
+
     return(
       <div>
         <div className="burger-bar"><Burger /></div>
         <div class="mx-auto w-75 bg-dark my-5 px-5 py-5">Feedback</div>
+        {temp};
       </div>
     );
   }
