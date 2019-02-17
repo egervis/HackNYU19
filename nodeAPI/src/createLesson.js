98809bb5ac6bcc8c4f9847e4fdc1a8b6dfb3d145
Edward Gervis <edward.gervis@gmail.com>
2019-02-16 20:56:40 -0500

    create lesson

--- nodeAPI/src/createLesson.js
+++ nodeAPI/src/createLesson.js
@@ -2,5 +2,30 @@
 import uniqid from 'uniqid';
 
 export const request = async (req, res, pool) => {
-
-}
+  try {
+    let pictures = req.body.allPictures;
+    let str = ""
+    foreach(pic in pictures)
+    {
+      let pictureID = uniqid();
+      let query = {
+        text:'INSERT INTO pictures(pictureID, pictureName, lessonId, pictureFile) VALUES($1, $2, $3, $4)' ,
+        values: [pictureID, pic.pictureName, req.body.lessonID, pic.pictureFile]
+      };
+      str+=(pictureID+",");
+    }
+    str = str.substring(0, str.length - 1);
+    const lessonId = uniqid();
+    let query = {
+      text:'INSERT INTO lessons(lessonId, lessonName, lessonDescription, pictureIds, instructorID) VALUES($1, $2, $3, $4, $5)' ,
+      values: [lessonId, req.body.lessonName, req.body.lessonDescription, str, req.body.instructorID]
+    };
+    await pool.query(query);
+    res.status(200).send(JSON.stringify({
+      lessonCode: lessonsId
+    }));
+  } catch (error) {
+    console.error('ERROR creating class', error.stack);
+    res.status(500).send({'error': error.stack});
+  }
+};
