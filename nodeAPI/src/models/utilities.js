3d4f09bdcbd62fb6cb645923030bc18765b357f1
Brian Chiu <bkchiu0@gmail.com>
2019-03-04 21:56:44 -0500

    Update utilities.js

--- nodeAPI/src/models/utilities.js
+++ nodeAPI/src/models/utilities.js
@@ -1,15 +1,15 @@
 "use strict";
 
 // Converts a string of stuff separated by commas to an array.
-export const convertStringToArray(str) {
+export const convertStringToArray = (str) => {
   return str.split(',');
-}
+};
 
 // Converts an array of stuff to a comma separated string.
-export const convertArrayToString(arr) {
+export const convertArrayToString = (arr) => {
   let output = '';
-  for(int i=0; i<arr.length-1; i++) {
+  for(let i=0; i<arr.length-1; i++) {
     output = output.concat(arr[i]).concat(',');
   }
   return output.concat(arr[arr.length-1]);
-}
+};
