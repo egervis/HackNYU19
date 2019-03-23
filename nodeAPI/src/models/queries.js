2a3df9b243cfc56118d10833e9fc10c41eb37733
Edward Gervis <edward.gervis@gmail.com>
2019-03-23 18:56:23 -0400

    Update queries.js

--- nodeAPI/src/models/queries.js
+++ nodeAPI/src/models/queries.js
@@ -16,6 +16,26 @@ export const getUsersByID = (userid) => {
     values: [userid]
   };
   let user = await pool.query(query).rows[0];
-  userObjects = new User(user.userid, user.usertype, user.lastname, user.firstname, user.email, user.userpassword);
-  return userObjects;
+  userObject = new User(user.userid, user.usertype, user.lastname, user.firstname, user.email, user.userpassword);
+  return userObject;
+};
+
+/**
+ * Returns an array of User objects based on a given classID.
+ * @param  {string} classid The comma separated list string.
+ * @return {Array<User>} A User Object.
+ */
+export const getUsersByID = (classid) => {
+ let query = {
+   text: 'SELECT * FROM users INNER JOIN users ON classstudents.classid = $1',
+   values: [classid]
+ };
+ let users = await pool.query(query).rows;
+ let userObjects = [];
+ for(let i=0; i<users.length; i++)
+ {
+   let user = users[i];
+   userObjects.push(new User(user.userid, user.usertype, user.lastname, user.firstname, user.email, user.userpassword));
+ }
+ return userObjects;
 };
