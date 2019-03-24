ea2a288e21c7483a584dd1d926ab67f4394039eb
Edward Gervis <edward.gervis@gmail.com>
2019-03-23 20:12:29 -0400

    Create createEvent.js

---
+++ nodeAPI/src/events/createEvent.js
@@ -0,0 +1,48 @@
+"use strict";
+import uniqid from 'uniqid';
+
+/**
+ * Creates a class instance in the database.
+ * @param {Request} req  body: { className: string, instructorID: string }
+ * @param {Response} res
+ * @param {postgres.Pool} pool
+ * @return {Promise}  status: 200, 404, 500 & { classCode: string }
+ */
+export const request = async (req, res, pool) => {
+  try {
+    const eventId = uniqid();
+    let query = {
+      text:'INSERT INTO events (eventID, eventType, eventName, dateExpires, instructorID, classID) VALUES($1, $2, $3, $4, $5, $6)' ,
+      values: [eventId, req.body.eventType, req.body.eventName, req.body.dateExpires, req.body.instructorID, req.body.classID]
+    };
+    await pool.query(query);
+
+
+    let query2 = {
+      text: 'SELECT * FROM userclasses WHERE classid = $1',
+      values: [classid]
+    };
+    let users = await pool.query(query2).rows;
+    let userIDs = [];
+    for(let i=0; i<users.length; i++)
+    {
+      let user = users[i];
+      userIDs.push(user.userid);
+    }
+
+    for(let i=0; i<userIDs.length; i++)
+    {
+      let query3 = {
+        text:'INSERT INTO usersevents (userID ,eventID) VALUES($1, $2)' ,
+        values: [userIDs[i], eventId]
+      };
+    }
+    await pool.query(query3);
+
+
+    res.status(200).send();
+  } catch (error) {
+    console.error('ERROR creating event', error.stack);
+    res.status(500).send({'error': error.stack});
+  }
+};
