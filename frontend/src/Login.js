2926c21e47e7a6310364690cc57aff0bb0d83326
HKgem1314 <46288591+HKgem@users.noreply.github.com>
2019-06-05 14:37:53 -0400

    login ui update

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -56,7 +56,7 @@ class Login extends Component{
       return(
         <div class="container">
           <div class="row">
-            <div class="col-sm-9 col-md-7 mx-auto">
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
               <div class="card card-signin my-5">
                 <div class="card-body">
                   <h3 class="card-title text-center">Sign In</h3>
