4d04476fbc3d4139d74b5bd34dbe85b129ed634b
Brian Chiu <bkchiu0@gmail.com>
2019-06-09 12:12:33 -0400

    Fix dashboard api calls

--- frontend/src/teacher/classes.js
+++ frontend/src/teacher/classes.js
@@ -1,16 +1,13 @@
 import '../styles/teacherClasses.css';
 
-import React, {
-  useState,
-  useEffect
-} from 'react';
+import React, {useEffect, useState} from 'react';
+import {Button, Form, Loader} from 'semantic-ui-react';
 
-import BurgerMenu from '../components/Burger';
 import {
   classCreateRequestor,
-  getClassRequestor
+  getClassRequestor,
 } from '../requests/requestBuilder';
-import { Loader, Button, Form } from 'semantic-ui-react';
+import BurgerMenu from '../components/Burger';
 
 export const Dashboard = props => {
   const [isFormOpen, toggleForm] = useState(false);
@@ -19,44 +16,45 @@ export const Dashboard = props => {
   const [classes, setClasses] = useState([]);
 
   const createClass = () => {
-    classCreateRequestor(className, localStorage.getItem('userid'));
+    setLoading(true);
+    classCreateRequestor(className, localStorage.getItem('userid'))
+      .then(response => {
+        setClasses(
+          classes.push({
+            classid: response.data.classid,
+            classname: className,
+            instructorid: localStorage.getItem('userid'),
+          }),
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
     toggleForm(false);
   };
 
   const createForm = (
-    <Form inverted size='big'>
-        <Form.Field>
-        <Form.Input 
-          label='Class Name' 
-          placeholder='Please enter a class name'
-          onChange = {e => setClassName(e.target.value)}
-          width={8}/>
-        </Form.Field>
-        <Button
-          inverted
-          size='big'
-          type='submit'
-          onClick={createClass}
-        >
+    <Form inverted size="big">
+      <Form.Field>
+        <Form.Input
+          label="Class Name"
+          placeholder="Please enter a class name"
+          onChange={event => setClassName(event.target.value)}
+          width={8}
+        />
+      </Form.Field>
+      <Button inverted size="big" type="submit" onClick={createClass}>
         Submit
-        </Button>
-        <Button
-          inverted
-          size='big'
-          onClick={() => toggleForm(false)}
-        >
+      </Button>
+      <Button inverted size="big" onClick={() => toggleForm(false)}>
         Cancel
-        </Button>
+      </Button>
     </Form>
   );
 
   const createButton = (
-    <Button
-      inverted
-      size='big'
-      onClick={() => toggleForm(true)}
-    >
-    Create
+    <Button inverted size="big" onClick={() => toggleForm(true)}>
+      Create
     </Button>
   );
 
@@ -67,29 +65,28 @@ export const Dashboard = props => {
         // Sets the state to the data from the classes endpoint (class[])
         setClasses(response.data);
         setLoading(false);
-      }).catch(error => {
+      })
+      .catch(error => {
         // Error handler
         setLoading(false);
         console.error(error);
       });
   }, []);
 
-  return(
+  return (
     <div>
       <BurgerMenu />
-        <main id="page-wrap" className="w-75">
-          <div id="content">
-            <h2>Welcome to your dashboard.</h2>
-            {isFormOpen ? createForm : createButton}
-            {loading ? 
-              <Loader>Loader</Loader>
-              :
-              classes.map(c =>
-                <div>{c.classname}</div>
-              )
-            }
-          </div>
-        </main>
+      <main id="page-wrap" className="w-75">
+        <div id="content">
+          <h2>Welcome to your dashboard.</h2>
+          {isFormOpen ? createForm : createButton}
+          {loading ? (
+            <Loader>Loader</Loader>
+          ) : (
+            classes.map(cl => <div key={cl.classid}>{cl.classname}</div>)
+          )}
+        </div>
+      </main>
     </div>
   );
 };
